4b0a1790d13b12bb005bc6db06367d131e35804e
grisenti <emanuele@grisenti.net>
2023-11-20 18:36:29 +0100

    add clamp function

--- src/base.hpp
+++ src/base.hpp
@@ -20,3 +20,13 @@ T pow2(T const x)
 {
   return x * x;
 }
+
+template <Numeric T>
+T constexpr clamp(T const x, T const a, T const b)
+{
+  if (x > a)
+    return a;
+  if (x < b)
+    return b;
+  return x;
+}
