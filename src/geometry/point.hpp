76af781e575f2e7915cf9d03f4a89bb4001d4d94
grisenti <emanuele@grisenti.net>
2024-01-04 15:33:44 +0100

    add homogeneous point class

--- src/geometry/point.hpp
+++ src/geometry/point.hpp
@@ -64,11 +64,19 @@ Point2<ComponentT<M>> constexpr as_point2(M const &m)
 }
 
 template <typename T>
+struct HPoint3;
+
+template <typename T>
 struct Point3 : PointBase<3, T>
 {
   T x;
   T y;
   T z;
+
+  static Point3 from_homogeneous_point(HPoint3<T> const &p)
+  {
+    return Point3<T>{ p.x / p.w, p.y / p.w, p.z / p.w };
+  }
 
   constexpr Point3() = default;
 
@@ -99,12 +107,6 @@ Vec3<T> constexpr as_vec3(Point3<T> const &p)
   return Vec3<T>{ p.x, p.y, p.z };
 }
 
-template <typename T>
-Vec4<T> constexpr as_vec4(Point3<T> const &p)
-{
-  return Vec4<T>{ p.x, p.y, p.z, 1 };
-}
-
 template <Matrix1D M>
   requires(M::SIZE == 3)
 Point3<ComponentT<M>> constexpr as_point3(M const &m)
@@ -114,6 +116,49 @@ Point3<ComponentT<M>> constexpr as_point3(M const &m)
 
 using Point3f = Point3<Real>;
 using Point3i = Point3<int>;
+
+/// Homogeneous point in 3D space.
+template <typename T>
+struct HPoint3 : PointBase<4, T>
+{
+  T x;
+  T y;
+  T z;
+  T w;
+
+  constexpr HPoint3() = default;
+
+  constexpr HPoint3(T x, T y, T z, T w) : x(x), y(y), z(z), w(w)
+  {
+  }
+
+  constexpr HPoint3(Point3<T> const &p) : x(p.x), y(p.y), z(p.z), w(1)
+  {
+  }
+
+  T operator[](size_t i) const
+  {
+    static_assert(std::is_standard_layout_v<HPoint3<T>>,
+        "HPoint3 must be standard layout for operator[] to work");
+    return (&x)[i];
+  }
+
+  T &operator[](size_t i)
+  {
+    static_assert(std::is_standard_layout_v<HPoint3<T>>,
+        "HPoint3 must be standard layout for operator[] to work");
+    return (&x)[i];
+  }
+};
+
+using HPoint3f = HPoint3<Real>;
+
+template <Matrix1D M>
+  requires(M::SIZE == 4)
+HPoint3<ComponentT<M>> constexpr as_hpoint3(M const &m)
+{
+  return { m[0], m[1], m[2], m[3] };
+}
 
 template <Point P, Vector V>
   requires(P::SIZE == V::SIZE) && std::same_as<ComponentT<P>, ComponentT<V>>
