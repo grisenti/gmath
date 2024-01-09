1c4039918e1552395de44ab17db3615535f0f810
grisenti <emanuele@grisenti.net>
2024-01-09 08:25:29 +0100

    simplify extend and truncate functions

--- include/gmath/vec.hpp
+++ include/gmath/vec.hpp
@@ -11,58 +11,23 @@
 namespace gmath
 {
 
-template <typename T>
-Vec3<T> extend(Vec2<T> const &v, T z = {})
+template <Vector V>
+  requires(V::SIZE > 1)
+DefaultVectorOf<V::SIZE + 1, ComponentT<V>> extend(V const &v, ComponentT<V> value = {})
 {
-  return { v.x, v.y, z };
-}
-
-template <typename T>
-Vec2<T> truncate(Vec3<T> const &v)
-{
-  return { v.x, v.y };
-}
-
-template <typename T>
-Vec4<T> extend(Vec3<T> const &v, T w = {})
-{
-  return { v.x, v.y, v.z, w };
-}
-
-template <typename T>
-Vec3<T> truncate(Vec4<T> const &v)
-{
-  return { v.x, v.y, v.z };
-}
-
-template <typename T>
-VecN<5, T> extend(Vec4<T> const &v, T value = {})
-{
-  return { v.x, v.y, v.z, v.w, value };
-}
-
-template <typename T>
-Vec4<T> truncate(VecN<5, T> const &v)
-{
-  return { v[0], v[1], v[2], v[3] };
-}
-
-template <typename T, size_t N>
-VecN<N + 1, T> extend(VecN<N, T> const &v, T value = {})
-{
-  VecN<N + 1, T> res;
-  for (size_t i = 0; i < N; ++i)
+  DefaultVectorOf<V::SIZE + 1, ComponentT<V>> res;
+  for (size_t i = 0; i < V::SIZE; ++i)
     res[i] = v[i];
-  res[N] = value;
+  res[V::SIZE] = value;
   return res;
 }
 
-template <typename T, size_t N>
-  requires(N > 1)
-VecN<N - 1, T> truncate(VecN<N, T> const &v)
+template <Vector V>
+requires(V::SIZE > 1)
+DefaultVectorOf<V::SIZE - 1, ComponentT<V>> truncate(V const &v)
 {
-  VecN<N - 1, T> res;
-  for (size_t i = 0; i < N - 1; ++i)
+  DefaultVectorOf<V::SIZE - 1, ComponentT<V>> res;
+  for (size_t i = 0; i < V::SIZE - 1; ++i)
     res[i] = v[i];
   return res;
 }
