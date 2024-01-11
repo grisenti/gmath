97a64845d3947dfb5f27300340193fd8c8ed7a4b
grisenti <emanuele@grisenti.net>
2024-01-11 16:08:00 +0100

    formatting

--- include/gmath/geometry.hpp
+++ include/gmath/geometry.hpp
@@ -16,9 +16,9 @@ inline Line join(Point3f const &p1, Point3f const &p2)
 inline Plane join(Line const &L, Point3f const &p)
 {
   return {
-    Normal3f{L.direction.y * p.z - L.direction.z * p.y + L.moment.x,
+    Normal3f{ L.direction.y * p.z - L.direction.z * p.y + L.moment.x,
              L.direction.z * p.x - L.direction.x * p.z + L.moment.y,
-             L.direction.x * p.y - L.direction.y * p.x + L.moment.z},
+             L.direction.x * p.y - L.direction.y * p.x + L.moment.z },
     -dot(L.moment, as_vec3(p))
   };
 }
@@ -31,12 +31,12 @@ inline Plane join(Point3f const &p, Line const &L)
 inline Line meet(Plane const &f, Plane const &g)
 {
   return {
-    Vec3f{f.normal.y * g.normal.z - f.normal.z * g.normal.y,
+    Vec3f{ f.normal.y * g.normal.z - f.normal.z * g.normal.y,
           f.normal.z * g.normal.x - f.normal.x * g.normal.z,
-          f.normal.x * g.normal.y - f.normal.y * g.normal.x},
+          f.normal.x * g.normal.y - f.normal.y * g.normal.x },
     Normal3f{ g.normal.x * f.d - f.normal.x * g.d,
           g.normal.y * f.d - f.normal.y * g.d,
-          g.normal.z * f.d - f.normal.z * g.d              }
+          g.normal.z * f.d - f.normal.z * g.d               }
   };
 }
 
