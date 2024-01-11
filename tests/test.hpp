1b3a9bd5e059528dd4b6cbd23123812146814af9
grisenti <emanuele@grisenti.net>
2024-01-11 16:08:15 +0100

    fix printing functions in tests

--- tests/test.hpp
+++ tests/test.hpp
@@ -1,22 +1,35 @@
-#include <catch2/catch_all.hpp>
 #include <iostream>
+#include <catch2/catch_all.hpp>
 
 #include "gmath/vec.hpp"
 #include "gmath/mat.hpp"
 #include "gmath/formatting.hpp"
 
-using namespace gmath;
+namespace gmath
+{
 
-template <Vector V>
+template <Matrix1D V>
 std::ostream &operator<<(std::ostream &out, V const &v)
 {
-  out << std::format("{}", v);
-  return out;
+  return out << std::format("{}", v);
 }
 
 template <typename T, size_t R, size_t C>
 std::ostream &operator<<(std::ostream &out, Matrix<R, C, T> const &mat)
 {
-  out << std::format("{}", mat);
-  return out;
+  return out << std::format("{}", mat);
 }
+
+inline std::ostream &operator<<(std::ostream &out, Line const &l)
+{
+  return out << std::format("{}", l);
+}
+
+inline std::ostream &operator<<(std::ostream &out, Plane const &p)
+{
+  return out << std::format("{}", p);
+}
+
+}
+
+using namespace gmath;
