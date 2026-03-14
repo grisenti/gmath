7f48dd519f9e7d60a6ddd3adfcd316b1f32a5f86
grisenti <emanuele@grisenti.dev>
2026-03-14 19:17:05 +0100

    add constant

--- include/gmath/constants.hpp
+++ include/gmath/constants.hpp
@@ -8,7 +8,8 @@
 namespace gmath
 {
 
-auto constexpr C_PI = std::numbers::pi_v<Real>;
+auto constexpr PI = std::numbers::pi_v<Real>;
+auto constexpr INV_PI = 1 / PI;
 auto constexpr INFTY = std::numeric_limits<Real>::infinity();
 
 } // namespace gmath
